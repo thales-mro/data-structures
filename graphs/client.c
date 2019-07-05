88b227d024b60c333b67288fe9b0ef3f03420167
Thales <thalesteng@gmail.com>
2019-07-05 16:22:01 -0300

    Added basic skeleton for graphs

---
+++ graphs/client.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "graph.h"
+
+int main() {
+
+    return 0;
+}
---
+++ graphs/graph.h
@@ -0,0 +1,18 @@
+typedef struct Graph {
+    int **adj;
+    int n;
+} Graph;
+
+typedef Graph * graph_ptr;
+
+graph_ptr createGraph(int n);
+
+void destroyGraph(graph_ptr g);
+
+void insertEdge(graph_ptr g, int u, int v);
+
+void removeEdge(graph_ptr g, int u, int v);
+
+int hasEdge(graph_ptr g, int u, int v);
+
+void printEdges(graph_ptr g);
