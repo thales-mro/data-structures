bf1482f14304d738198b58e95aca295266021cb6
Thales <thalesteng@gmail.com>
2019-07-09 10:33:57 -0300

    Implemented simple client for graph library

--- graphs/client.c
+++ graphs/client.c
@@ -2,6 +2,15 @@
 #include "graph.h"
 
 int main() {
+    graph_ptr g;
 
+    printf("Please insert two integers, representing the number of vertices and the number of edges, followed by the edges: ");
+
+    g = readGraph();
+
+    printEdges(g);
+    printf("The most popular vertex is %d\n", mostPopular(g));
+
+    destroyGraph(g);
     return 0;
 }
